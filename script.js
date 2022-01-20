fb030bd623d48b21a174a84e18e5541221b2dce5
Monet Man <88108211+jpcisneros411@users.noreply.github.com>
2022-01-20 17:18:51 -0600

    working on inputs

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 var today = moment();
 console.log(today);
 $("#currentDay").text(today.format("MMM Do, YYYY"));
+
+let hoverTime = document.getElementById("timeColumn");
+let hoverHappening = document.getElementById("happening");
+let hoverSave = document.getElementById("saveColumn");
+
+hoverHappening.addEventListener("click", clickedHappening)
+
+function clickedHappening(){
+    
+}
