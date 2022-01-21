361a778375fd2d4a0c654d9a48522de46726882a
Monet Man <88108211+jpcisneros411@users.noreply.github.com>
2022-01-21 16:49:05 -0600

    final commit working

--- script.js
+++ script.js
@@ -1,54 +1,53 @@
-
+let container = $('.container');
+let times = ['9:00am', '10:00am', '11:00am', '12:00pm', '1:00pm', '2:00pm', '3:00pm', '4:00pm', '5:00pm'];
+let jumbo = $('.jumbotron');
 var today = moment();
-console.log(today);
 $("#currentDay").text(today.format("MMM Do, YYYY"));
-//var inputData = document.querySelector(".inputData");
-//let happening = document.querySelector(".happening");
-var inputData = document.getElementsByClassName("inputData");
-console.log(inputData);
-let hoverTime = document.getElementsByClassName("timeColumn");
-// let happening = document.querySelectorAll(".happening").forEach(item => {
-//     item.addEventListener("click", clickedHappening => {
-//       //handle click
-//       console.log("potato");
-//       //console.log(clickedHappening);
-//       //let happening = document.getElementById(".inputData").style.display=inside;
-//     })
-//   })
 
-$(document).ready(function(){
-    $('.happening').on('click',function(){
-        alert('you have clicked!');
-        //$(this).css('display','flex');
-        $(this).querySelector(".inputData").css('display','flex');
-        console.log("potato");
+
+//loop that goes through hours
+for (let i = 0; i < times.length; i++) {
+    //hour array
+    let time = times[i];
+    
+    //hours logic
+    let rowSection = $('<section>').addClass('row rounded align-items-center');
+    let hours = $('<div>').addClass('time-block col text-center');
+    hours.text(time);
+    rowSection.append(hours);
+
+    //save button
+    let saveThis = $('<div>').addClass('col-lg-3 col text-center align-items-center');
+    let savingBtn = $('<button>').addClass('savingBtn');
+    savingBtn.attr('date-time', time);
+    savingBtn.text('save');
+    saveThis.append(savingBtn);
+    rowSection.append(saveThis);
+    container.append(rowSection);
+    savingBtn.on("click", function(){
+        localStorage.setItem(time, input.val())
     });
-});
-
-//let happening = document.querySelectorAll(".happening").forEach(item => {
-///let happening = document.getElementsByClassName("happening").forEach(item => {
-   /// $(happening).click(function(event){
-//$('a.some-class').click(function() {
-    //     switch (this.class) {
-    //         case 'one':
-    //             console.log("potato");
-    //             dataGiven;
-    //           break;
-    //         // case 'two':
-    //         //   // do something
-    //         //   break;
-    //         // default:
-    //         //   // do something
-    //         //   break;
-    //     }
-    // }
-    // )};
-let hoverSave = document.getElementsByClassName("saveColumn");
-var clickedSlot;
-var clickedHappening;
-//clickedSlot = happening.addEventListener("click", clickedHappening);
-//inputData.removeAttribute("class");
-//function clickedHappening(){
-    //console.log("fajitas");
- //inputData.setAttribute("class","dataGiven");
-//};
+    
+    //rolling times
+    let now = moment();
+    moment().hour();
+    let thisHour = moment().hour();
+    let hourNumber = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+    if (thisHour === hourNumber[i]){
+        rowSection.css('background-color', '#ff0000');
+    } else if (thisHour < hourNumber[i]) {
+        rowSection.css('background-color', '#00FF00');
+    } else if (thisHour > hourNumber[i]) {
+        rowSection.css('background-color', '#C0C0C0');
+    }
+    
+    //text input
+    let inputs = $('<div>').addClass('col-lg-6 col text-center');
+    let input = $('<textarea>');
+    $(this).attr('cols', '50')
+    input.attr('id', time);
+    inputs.append(input);
+    rowSection.append(inputs);
+    let savedText = localStorage.getItem(time);
+    input.val(savedText);
+}
