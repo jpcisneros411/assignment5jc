ee0767b6f1e41253d09fbbd602e25c32ba7091a3
Monet Man <88108211+jpcisneros411@users.noreply.github.com>
2022-01-20 20:38:27 -0600

    eventlistener

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 var today = moment();
 console.log(today);
 $("#currentDay").text(today.format("MMM Do, YYYY"));
-var inputData = document.querySelector(".inputData");
+//var inputData = document.querySelector(".inputData");
+var inputData = document.getElementsByClassName("inputData");
+console.log(inputData);
 let hoverTime = document.getElementById("timeColumn");
-let hoverHappening = document.getElementById("happening");
+let happening = document.getElementById("happening");
 let hoverSave = document.getElementById("saveColumn");
-
-//hoverHappening.addEventListener("click", clickedHappening)
-inputData.removeAttribute("class");
-// function clickedHappening(){
-
-// }
+//var clickedSlot;
+//clickedHappening;
+clickedSlot = happening.addEventListener("click", clickedHappening);
+//inputData.removeAttribute("class");
+function clickedHappening(){
+ inputData.setAttribute("class","dataGiven");
+ console.log(potato);
+}
