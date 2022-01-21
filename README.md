a6024a966ca6f0738687a09d04b64009b07ffd23
Monet Man <88108211+jpcisneros411@users.noreply.github.com>
2022-01-20 22:02:59 -0600

    update

--- README.md
+++ README.md
@@ -18,14 +18,14 @@ SO THAT I can manage my time effectively
 
 ```md
 GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
+WHEN I open the planner 
+THEN the current day is displayed at the top of the calendar -c/w
+WHEN I scroll down 
 THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I view the timeblocks for that day - c/w
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future -working
 WHEN I click into a timeblock
-THEN I can enter an event
+THEN I can enter an event - working
 WHEN I click the save button for that timeblock
 THEN the text for that event is saved in local storage
 WHEN I refresh the page
--- script.js
+++ script.js
@@ -2,15 +2,16 @@ var today = moment();
 console.log(today);
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 //var inputData = document.querySelector(".inputData");
+//let happening = document.querySelector(".happening");
 var inputData = document.getElementsByClassName("inputData");
 console.log(inputData);
 let hoverTime = document.getElementsByClassName("timeColumn");
-//let happening = document.querySelector(".happening");
 let happening = document.querySelectorAll(".happening").forEach(item => {
     item.addEventListener("click", clickedHappening => {
       //handle click
       console.log("potato");
       console.log(clickedHappening);
+      
     })
   })
 let hoverSave = document.getElementsByClassName("saveColumn");
@@ -18,7 +19,7 @@ var clickedSlot;
 var clickedHappening;
 //clickedSlot = happening.addEventListener("click", clickedHappening);
 //inputData.removeAttribute("class");
-function clickedHappening(){
-    console.log("potato");
+//function clickedHappening(){
+    //console.log("fajitas");
  //inputData.setAttribute("class","dataGiven");
-}
+//}
