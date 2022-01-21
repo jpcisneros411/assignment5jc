1d2c5591247a8a7d379d1c9bd4f57c2838fb4f91
Monet Man <88108211+jpcisneros411@users.noreply.github.com>
2022-01-20 19:39:42 -0600

    removing Attributes

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 var today = moment();
 console.log(today);
 $("#currentDay").text(today.format("MMM Do, YYYY"));
-
+var inputData = document.querySelector(".inputData");
 let hoverTime = document.getElementById("timeColumn");
 let hoverHappening = document.getElementById("happening");
 let hoverSave = document.getElementById("saveColumn");
 
-hoverHappening.addEventListener("click", clickedHappening)
+//hoverHappening.addEventListener("click", clickedHappening)
+inputData.removeAttribute("class");
+// function clickedHappening(){
 
-function clickedHappening(){
-    
-}
+// }
